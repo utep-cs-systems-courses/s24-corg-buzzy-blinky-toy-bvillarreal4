1d329e19e77d4e4ff1c01076ac7f20066d01cd5c
bvillarreal4 <bvillarreal4@miners.utep.edu>
2024-04-25 17:30:35 -0600

    worked on buttons

--- project/main.c
+++ project/main.c
@@ -4,16 +4,19 @@
 #include "buzzer.h"
 
 #define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
 
+#define switches (SW1 | SW2 | SW3)
 int main(void){
 
   configureClocks();
   buzzer_init();
 
-  P2REN |= SW1;
-  P2IE |= SW1;
-  P2OUT |= SW1;
-  P2DIR &= ~SW1;
+  P2REN |= switches;
+  P2IE |= switches;
+  P2OUT |= switches;
+  // P2DIR &= ~SW1;
 
   or_sr(0x18);
 
@@ -24,12 +27,20 @@ void __interrupt_vec(PORT2_VECTOR) Port_2(){
 
   if(P2IFG & SW1){
     P2IFG &= ~SW1;
-    switch_interrupt_handler();
+    switch_interrupt_handler_SW1();
+  }
+  if(P2IFG & SW2){
+    P2IFG &= ~SW2;
+    switch_interrupt_handler_SW2();
+  }
+  if(P2IFG & SW3){
+    P2IFG &= ~SW3;
+    switch_interrupt_handler_SW3();
   }
 
 }
 
-void switch_interrupt_handler(){
+void switch_interrupt_handler_SW1(){
   char p2 = P2IN;
 
   P2IES |= (p2 & SW1);
@@ -41,7 +52,50 @@ void switch_interrupt_handler(){
   else{
     buzzer_set_period(632);
   }
+}
+
+
+void switch_interrupt_handler_SW2(){
+  char p2 = P2IN;
+
+  P2IES |= (p2 & SW2);
+  P2IES &= (p2 | ~SW2);
+
+  if(p2 & SW2){
+    buzzer_set_period(0);
+  }
+  else{
+    buzzer_set_period(554);
+  }
+}
+
+void switch_interrupt_handler_SW3(){
+  char p2 = P2IN;
+
+  P2IES |= (p2 & SW3);
+  P2IES &= (p2 | ~SW3);
+
+  const int E = 632;
+  const int D = 55;
+  const int A = 440;
+
+  int song[] = {E,D,A,E,D,A,E,D,A,E,D,A,E,D,E,D,E,D,A,E,D,A,E,D,A,E,E,E,E,D,A,D};
+  int count = 0;
+  int limit = sizeof(song)/sizeof(song[0]);
+  int interruptCount = 0;
+
+  if(p2 & SW3){
+    buzzer_set_period(0);
+  }
+  else{
+    buzzer_set_period(632);
+    __delay_cycles(1000000);
+    buzzer_set_period(550);
+    __delay_cycles(1000000);
+    buzzer_set_period(440);
+    __delay_cycles(1000000);
+  }
+				      
 
   
-
 }
